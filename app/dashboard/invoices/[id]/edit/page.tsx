efac2718cd5415d5b3fd9593e730030ad8e0649c
gabriel2426 <81568682+gabriel2426@users.noreply.github.com>
2026-05-28 01:18:15 +0700

    feat: add try/catch error handling, form validation, error.tsx, not-found.tsx, ESLint config

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,22 @@
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import EditInvoiceForm from '@/app/ui/invoices/edit-form';
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+}
